bafad705eea57af83eb23d3ade79b2832e5a147f
Dat1810 <94735394+xDatx@users.noreply.github.com>
2023-12-01 21:02:19 +0700

    Add files via upload

---
+++ WindowRect/WindowRect/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowRect
+{
+     class Program
+    {
+        [DllImport("user32.dll")]
+        static extern IntPtr FindWindow(string lpClassName, string lpWindowName);
+
+        [DllImport("user32.dll")]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        static extern bool GetClientRect(IntPtr hwnd, out RECT rect);
+
+        [DllImport("user32.dll")]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        static extern bool ClientToScreen(IntPtr hwnd, ref POINT rect);
+
+        public static RECT rectT;
+        //Make The RECT and Point
+        public struct POINT
+        {
+            public int x;
+            public int y;
+        }
+
+        public struct RECT
+        {
+            public int left;
+            public int top;
+            public int right;
+            public int bottom;
+        }
+
+        
+        static void Main(string[] args) 
+        {
+            RenderTest test = new RenderTest();
+            test.Start();
+            string windowTitle = "Untitled - Notepad";
+
+            while (true)
+            {
+                IntPtr Hwnd = FindWindow(null, windowTitle);
+                if (Hwnd != IntPtr.Zero)
+                {
+                    GetClientRect(Hwnd, out RECT rect);
+                    //Use Point to get
+                    POINT TopLeft = new POINT { x = rect.left, y = rect.top };
+                    ClientToScreen(Hwnd, ref TopLeft);
+
+                    rectT = new RECT { left = TopLeft.x, top = TopLeft.y, bottom = rect.bottom, right = rect.right };
+                }
+            }
+            
+        }
+    }
+}
---
+++ WindowRect/WindowRect/RenderTest.cs
@@ -0,0 +1,18 @@
+﻿using ImGuiNET;
+using ClickableTransparentOverlay;
+using System.Numerics;
+using static WindowRect.Program;
+namespace WindowRect
+{
+    class RenderTest : Overlay
+    {
+        protected override void Render()
+        {
+            ImGui.SetNextWindowPos(new Vector2(rectT.left, rectT.top));
+            ImGui.SetNextWindowSize(new Vector2(rectT.right, rectT.bottom));
+            ImGui.Begin("Test");
+            ImGui.End();
+            
+        }
+    }
+}
